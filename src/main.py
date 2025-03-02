bc4a9d31845f08c9a933e4143410ae07a2d00000
Leezy <loahnisk@gmail.com>
2025-03-02 09:43:59 -0600

    added test mardown file for generate websites

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
+import os
+import shutil
+
 from block_markdown import markdown_to_blocks, block_to_block_type
 from inline_markdown import split_nodes_images
 from textnode import *
 from generate_website import *
 
 def main():
-    copy_static("static", "public", True)
+    
+    header_text = extract_title("src/test.md")
+    print(header_text)
 
 if __name__ == "__main__":
     main()
