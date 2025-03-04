707d22fea34caf001bd5f4e7ffbb082c7ba3cc96
Leezy <loahnisk@gmail.com>
2025-03-04 03:32:38 -0600

    deleted public folder and added docs folder to hold contents

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from block_markdown import markdown_to_blocks, block_to_block_type
 from inline_markdown import split_nodes_images
@@ -8,11 +9,12 @@ from generate_website import *
 
 def main():
     
-    copy_static("static", "public")
+    copy_static("static", "docs")
     # Use sys.argv to grab the first CLI argument
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    generate_pages_recursive("content", "template.html", "public", basepath)
+        print(f"Basepath: {basepath}")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
